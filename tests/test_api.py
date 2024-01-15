f9ec761d300159eaed974e76f0040e4a8b035883
Sean <63349506+SerRichard@users.noreply.github.com>
2024-01-15 16:41:55 +0100

    [WIP] Basic Api and Client classes (#5)
    * accept reformatting
    
    * accept reformatting
    
    * edit readme
    
    * accept reformatting
    
    * remove unused imports
    
    * accept reformatting
    
    * accept reformatting
    
    ---------
    
    Co-authored-by: sean <sean.hoyal@external.eodc.eu>

---
+++ tests/test_api.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from openeo_fastapi.api.app import OpenEOApi
+
+
+def test_api_core(core_api):
+    """Test the OpenEOApi and OpenEOCore classes interact as intended."""
+
+    assert isinstance(core_api, OpenEOApi)
+    assert isinstance(core_api.app, FastAPI)
+
+
+def test_get_capabilities(core_api):
+    """Test the OpenEOApi and OpenEOCore classes interact as intended."""
+
+    test_app = TestClient(core_api.app)
+
+    core_api.register_get_capabilities()
+
+    response = test_app.get("/")
+
+    assert response.status_code == 200
+    assert response.json()["title"] == "Test Api"
+
+
+def test_get_conformance(core_api):
+    """Test the OpenEOApi capabilities endpoint is activate."""
+
+    test_app = TestClient(core_api.app)
+
+    core_api.register_get_conformance()
+
+    response = test_app.get("/conformance")
+
+    assert response.status_code == 200
+
+
+def test_get_conformance(core_api):
+    """Test the OpenEOApi capabilities endpoint is activate."""
+
+    test_app = TestClient(core_api.app)
+
+    core_api.register_well_known()
+
+    response = test_app.get("/.well-known/openeo")
+
+    assert response.status_code == 200
--- tests/test_base.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-
-def test_setup():
-    assert True
