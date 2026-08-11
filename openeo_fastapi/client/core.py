31307c5c60fa3a6316a7fe9a131f65ba017ed788
Gerald Walter Irsiegler <gerald.irsiegler@eodc.eu>
2026-08-11 14:52:21 +0200

    Add: required type to capability endpoint, bump version

--- openeo_fastapi/client/core.py
+++ openeo_fastapi/client/core.py
@@ -108,6 +108,7 @@ class OpenEOCore:
             id=self._id,
             title=self.settings.API_TITLE,
             stac_version=self.settings.STAC_VERSION,
+            type="Catalog"
             api_version=self.settings.OPENEO_VERSION,
             description=self.settings.API_DESCRIPTION,
             backend_version=self.settings.OPENEO_VERSION,
